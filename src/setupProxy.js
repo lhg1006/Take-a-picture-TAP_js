5a429aa2ded199323046301cfbaf51993dff8b6a
이효규 <lhg961006@gmail.com>
2024-01-08 17:01:05 +0900

    env 추가

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:6060',
+            target: `${process.env.REACT_APP_MY_LOCAL_IP}:6060`,
             changeOrigin: true,
         })
     );
